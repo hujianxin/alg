12425da1819db99a13665b093120bc07a4f2f35f
hujianxin <hujianxin@xiaomi.com>
2020-03-12 21:52:45 +0800

    multiple pack problem

--- 3.py
+++ 3.py
@@ -5,10 +5,13 @@ N, V = map(int, input().split())
 # 与01背包的区别是，在压缩为1维的情况下，完全背包是从小到大遍历体积
 dp = [0 for _ in range(V + 1)]
 
+def complete_pack(v, w) -> None:
+    for i in range(v, V + 1):
+        dp[i] = max(dp[i], dp[i-v] + w)
+
 for i in range(N):
     v, w = list(map(int, input().split()))
-    for j in range(v, V + 1):
-        dp[j] = max(dp[j], dp[j - v] + w)
+    complete_pack(v, w)
 
 
 print(dp[V])
---
+++ 4.py
@@ -0,0 +1,32 @@
+N, V = map(int, input().split())
+
+dp = [0 for _ in range(V + 1)]
+
+
+def complete_pack(v, w) -> None:
+    for i in range(v, V + 1):
+        dp[i] = max(dp[i], dp[i - v] + w)
+
+
+def zero_one_pack(v, w) -> None:
+    for i in range(V, v - 1, -1):
+        dp[i] = max(dp[i], dp[i - v] + w)
+
+
+def multiple_pack(v, w, amount) -> None:
+    if v * amount >= V:
+        complete_pack(v, w)
+        return
+    k = 1
+    while k < amount:
+        zero_one_pack(k * v, k * w)
+        amount -= k
+        k *= 2
+    zero_one_pack(amount * v, amount * w)
+
+
+for i in range(N):
+    v, w, amount = list(map(int, input().split()))
+    multiple_pack(v, w, amount)
+
+print(dp[-1])
